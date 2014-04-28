7dca09116b595c613d8aace122e78f99686ca963
nimmicv <nimmicv@gmail.com>
2014-04-27 17:03:38 -0700

    ConsistentCache

--- client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
+++ client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
@@ -1,19 +1,42 @@
 package edu.sjsu.cmpe.cache.client;
 
+
+import java.util.List;
+
+import com.google.common.collect.Lists;
+import com.google.common.hash.Hashing;
+
 public class Client {
 
     public static void main(String[] args) throws Exception {
         System.out.println("Starting Cache Client...");
-        CacheServiceInterface cache = new DistributedCacheService(
+        CacheServiceInterface cache1 = new DistributedCacheService(
                 "http://localhost:3000");
+        CacheServiceInterface cache2 = new DistributedCacheService(
+                "http://localhost:3001");
+        CacheServiceInterface cache3 = new DistributedCacheService(
+                "http://localhost:3002");
+        
+        List<CacheServiceInterface> servers = Lists.newArrayList(cache1,cache2,cache3);
+        int x= 1;
+        char c ='a';
+        for(int i=1;i<=10;i++)
+        {
+        	int bucket = Hashing.consistentHash(Hashing.md5().hashString(Integer.toString(i)), servers.size());
+        	servers.get(bucket).put(i, Character.toString(c));
+        	
+          System.out.println("put("+i +"=>"+ c+")");
+          c++;
+        }
+        for(int i=1;i<=10;i++)
+        {
+        	int bucket = Hashing.consistentHash(Hashing.md5().hashString(Integer.toString(i)), servers.size());
+        	//System.out.println(servers.get(bucket).get(i));
+        	System.out.println("get("+i +") =>"+ servers.get(bucket).get(i));
+        }
+        
 
-        cache.put(1, "foo");
-        System.out.println("put(1 => foo)");
-
-        String value = cache.get(1);
-        System.out.println("get(1) => " + value);
-
-        System.out.println("Existing Cache Client...");
+        
     }
 
 }
